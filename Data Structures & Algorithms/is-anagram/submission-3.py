4439dc0d72fac37577e9816c002079bbb7881b53
Remya <rnair7147@gmail.com>
2026-04-20 15:44:38 -0400

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # s_lst = []
+        # t_lst = []
+        # for char in s:
+        #     s_lst.append(char)
+        # for char in t:
+        #     t_lst.append(char)
+
+        # s_lst.sort()
+        # t_lst.sort()
+
+        # return s_lst == t_lst
+        return sorted(s) == sorted(t)
+         
